ca0e2692c0ff7bda31f8cd10b1539cd4aaa84228
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-07-04 18:40:17 -0300

    my profile page

---
+++ script.js
@@ -0,0 +1,14 @@
+const carregar = () => {
+  const msg = window.document.getElementById('msg');
+  const hora = new Date().getHours();
+  let apresentacao = '';
+  if (hora >= 0 && hora < 12) {
+    apresentacao = "Bom dia";
+  } else if (hora >= 12 && hora < 18) {
+    apresentacao = "Boa Tarde";
+  } else {
+    apresentacao = "Boa Noite";
+  }
+
+  msg.innerHTML = `${apresentacao}, sou Raphael Amorim Schmidt.`;
+};
